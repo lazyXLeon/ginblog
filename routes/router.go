7aa0118393eb226a309e6ccaee3f662f39e342b6
lazyXLeon <3277496891@qq.com>
2024-10-28 15:11:09 +0800

    初始化项目+配置参数

---
+++ routes/router.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"ginblog/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() {
+	gin.SetMode(utils.AppMode)
+	r := gin.Default()
+
+	router := r.Group("api/v1")
+	{
+		router.GET("hello", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "ok",
+			})
+		})
+	}
+
+	r.Run(utils.HttpPort)
+}
