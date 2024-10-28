e33c1bafccdd6bef4430c0ced1e06047f8b882e8
lazyXLeon <3277496891@qq.com>
2024-10-28 23:19:44 +0800

    初步完成添加用户和查询用户接口&分页功能

--- model/User.go
+++ model/User.go
@@ -1,6 +1,10 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"ginblog/utils/errmsg"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
@@ -8,4 +12,33 @@ type User struct {
 	Password string `gorm:"type:varchar(20);not null" json:"password"`
 	Role     int    `gorm:"type:int" json:"role"`
 	//Avatar   string
+}
+
+// 查询用户是否存在
+func CheckUser(name string) (code int) {
+	var users User
+	db.Select("id").Where("username = ?", name).First(&users)
+	if users.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED // 1001
+	}
+	return errmsg.SUCCESS
+}
+
+// 新增用户
+func CreateUser(data *User) int {
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR //500
+	}
+	return errmsg.SUCCESS //200
+}
+
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	//var total int64
+	err = db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error
+	if err != nil && !errors.Is(gorm.ErrRecordNotFound, err) {
+		return nil
+	}
+	return users
 }
