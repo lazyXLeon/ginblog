b9e7ddfc8e0b18695b545f365ea2c42a8f39a9d7
lazyXLeon <3277496891@qq.com>
2024-10-31 23:02:23 +0800

    完成登录功能

---
+++ middleware/jwt.go
@@ -0,0 +1,97 @@
+package middleware
+
+import (
+	"ginblog/utils"
+	"ginblog/utils/errmsg"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var JwtKey = []byte(utils.JwtKey)
+var code = errmsg.SUCCESS
+
+type MyClaims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+// 生成token
+func SetToken(username string) (string, int) {
+	expireTime := time.Now().Add(10 * time.Hour)
+	SetClaims := MyClaims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    "ginblog",
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, SetClaims).SignedString(JwtKey)
+	if err != nil {
+		return "", errmsg.ERROR
+	}
+	return token, errmsg.SUCCESS
+}
+
+// 验证token
+func CheckToken(token string) (*MyClaims, int) {
+	setToken, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+	if key, _ := setToken.Claims.(*MyClaims); setToken.Valid {
+		return key, errmsg.SUCCESS
+	} else {
+		return nil, errmsg.ERROR
+	}
+}
+
+// jwt中间件
+func JwtToken() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenHeader := c.Request.Header.Get("Authorization")
+
+		if tokenHeader == "" {
+			code = errmsg.ERROR_TOKEN_NOT_EXIST
+			c.JSON(http.StatusOK, gin.H{
+				"code": code,
+				"msg":  errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
+		}
+		checkToken := strings.SplitN(tokenHeader, " ", 2)
+		if len(checkToken) != 2 || checkToken[0] != "Bearer" {
+			code = errmsg.ERROR_TOKEN_TYPE_WRONG
+			c.JSON(http.StatusOK, gin.H{
+				"code": code,
+				"msg":  errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
+		}
+		key, tCode := CheckToken(checkToken[1])
+		if tCode != errmsg.SUCCESS {
+			code = errmsg.ERROR_TOKEN_WRONG
+			c.JSON(http.StatusOK, gin.H{
+				"code": code,
+				"msg":  errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
+		}
+		if time.Now().Unix() > key.ExpiresAt {
+			code = errmsg.ERROR_TOKEN_EXPIRED
+			c.JSON(http.StatusOK, gin.H{
+				"code": code,
+				"msg":  errmsg.GetErrMsg(code),
+			})
+			c.Abort()
+			return
+		}
+		c.Set("username", key.Username)
+		c.Next()
+	}
+}
--- routes/router.go
+++ routes/router.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	v1 "ginblog/api/v1"
+	"ginblog/middleware"
 	"ginblog/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -10,25 +11,30 @@ func InitRouter() {
 	gin.SetMode(utils.AppMode)
 	r := gin.Default()
 
-	routerV1 := r.Group("api/v1")
+	auth := r.Group("api/v1")
+	auth.Use(middleware.JwtToken())
 	{
 		//用户模块的路由接口
+		auth.PUT("user/:id", v1.EditUser)
+		auth.DELETE("user/:id", v1.DeleteUser)
+		//分类模块的路由接口
+		auth.POST("category/add", v1.AddCate)
+		auth.PUT("category/:id", v1.EditCate)
+		auth.DELETE("category/:id", v1.DeleteCate)
+		//文章模块的路由接口
+		auth.POST("article/add", v1.AddArt)
+		auth.PUT("article/:id", v1.EditArt)
+		auth.DELETE("article/:id", v1.DeleteArt)
+	}
+	routerV1 := r.Group("api/v1")
+	{
 		routerV1.POST("user/add", v1.AddUser)
 		routerV1.GET("users", v1.GetUsers)
-		routerV1.PUT("user/:id", v1.EditUser)
-		routerV1.DELETE("user/:id", v1.DeleteUser)
-		//分类模块的路由接口
-		routerV1.POST("category/add", v1.AddCate)
 		routerV1.GET("category", v1.GetCate)
-		routerV1.PUT("category/:id", v1.EditCate)
-		routerV1.DELETE("category/:id", v1.DeleteCate)
-		//文章模块的路由接口
-		routerV1.POST("article/add", v1.AddArt)
 		routerV1.GET("article", v1.GetArt)
 		routerV1.GET("article/info/:id", v1.GetArtInfo)
 		routerV1.GET("article/list/:id", v1.GetCateArt)
-		routerV1.PUT("article/:id", v1.EditArt)
-		routerV1.DELETE("article/:id", v1.DeleteArt)
+		routerV1.POST("login", v1.Login)
 	}
 
 	err := r.Run(utils.HttpPort)
